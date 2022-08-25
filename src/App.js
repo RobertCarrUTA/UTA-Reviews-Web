4b8a48a59a9bf829500d2cf984490f109efa5209
Robert Carr <robertcarrcisd@gmail.com>
2022-08-25 18:39:17 -0500

    Minor additions to home screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+
 import NavScroll from "./components/Navbar.js";
+import Home from "./components/Home.js";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <NavScroll></NavScroll>;
+  return (
+    <div>
+      <NavScroll />
+      <Home></Home>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <body>
+      <section id="heading_subheading">
+        <div id="heading">
+          UTA Reviews
+          <div id="subheading">
+            Ever wanted more out of RateMyPRofessors? We aim to bring you more
+            information about your professors or staff members at UTA.
+          </div>
+        </div>
+      </section>
+      <section id="intro">
+        <div id="introText">
+          My name is Robert Carr, I am an undergraduate Computer Science student
+          at the University of Texas at Arlington. As a student, I was annoyed
+          by the lack of reviews and the lack of detail in the online reviews.
+        </div>
+      </section>
+    </body>
+  );
+};
+
+export default Home;
